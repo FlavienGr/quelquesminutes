382df523a4be9bc65c2b846bd34beaa8a03a73d3
Flavor85 <flaviengr75@gmail.com>
2019-05-27 18:04:46 +0200

    route get/post signup and ejs page signup added

--- router/auth.js
+++ router/auth.js
@@ -11,6 +11,25 @@ router.get('/login', (req, res) => {
   res.send({ test: 'ok' });
 });
 
+router.get('/signup', (req, res) => {
+  res.render('signupPage', {
+    pageTitle: 'Signup',
+  });
+});
+router.get('/signup/users', (req, res) => {
+  res.render('signupUsers', {
+    pageTitle: 'Signup',
+  });
+});
+router.get('/signup/associations', (req, res) => {
+  res.render('signupAssociations', {
+    pageTitle: 'Signup',
+  });
+});
+router.post('/signup/users', (req, res) => {
+  res.send('<h1>User signup</h1>');
+});
+
 router.get('/logout', (req, res) => {
   req.session.destroy();
   res.redirect('/');

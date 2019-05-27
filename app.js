9c50082ecc127600bec62af39e4d65ea3215793d
Flavor85 <flaviengr75@gmail.com>
2019-05-27 11:25:41 +0200

    added eslint prettier

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 
-app.get("*", (req, res) => {
-  res.send({ test: "ok" });
+app.get('*', (req, res) => {
+  res.send({ test: 'ok' });
 });
 
-////////
+// //////
 
 const PORT = process.env.PORT || 3090;
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Listen on port: ${PORT}`);
 });

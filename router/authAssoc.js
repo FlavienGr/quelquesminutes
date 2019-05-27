4e0aaff095d557bc27c7b447668c26da8879639b
Flavor85 <flaviengr75@gmail.com>
2019-05-27 18:51:25 +0200

    split routes users and assoc

---
+++ router/authAssoc.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/signup/associations', (req, res) => {
+  res.render('signupAssociations', {
+    pageTitle: 'Signup',
+  });
+});
+
+module.exports = router;

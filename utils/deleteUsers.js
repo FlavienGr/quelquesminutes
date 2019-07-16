75a1aac96e5daf72e4320ef2a18acc7d0a629205
Flavor85 <flaviengr75@gmail.com>
2019-07-16 23:06:24 +0200

    npm deleteUsers by mongodb:cypress

---
+++ utils/deleteUsers.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const { MONGO_DB_URI } = process.env;
+
+mongoose.connect(MONGO_DB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('mongodb connected');
+});
+
+const User = require('../models/User');
+
+const deleteUsers = async () => {
+  console.log('exec');
+  console.log(process.NODE_ENV, 'nodeenv');
+  await User.deleteMany();
+  return db.close(err => console.log(err));
+};
+deleteUsers();

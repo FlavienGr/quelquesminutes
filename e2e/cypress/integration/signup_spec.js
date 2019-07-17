ab41d308c67af82b8bf576d58b0f8c5c94107644
Flavor85 <flaviengr75@gmail.com>
2019-07-17 16:39:21 +0200

    context added signup_spec

--- e2e/cypress/integration/signup_spec.js
+++ e2e/cypress/integration/signup_spec.js
@@ -6,13 +6,28 @@ describe('Signup page => elements and signup', () => {
   it('Should contains a form', () => {
     cy.get('form');
   });
-  it('should register an user', () => {
-    cy.fixture('user.json').then((user) => {
-      cy.get('input[name=username]').type(user.username);
-      cy.get('input[name=email]').type(user.email);
-      cy.get('input[name=password]').type(user.password);
-      cy.get('.button_signup_page').click();
-      cy.contains('Account');
+  context('User registration => ', () => {
+    it('Fill out form, send data and find account link', () => {
+      cy.fixture('user.json').then((user) => {
+        cy.get('input[name=username]').type(user.username);
+        cy.get('input[name=email]').type(user.email);
+        cy.get('input[name=password]').type(user.password);
+        cy.get('.button_signup_page').click();
+        cy.contains('Account');
+      });
+    });
+  });
+  context('Association registration => ', () => {
+    it('Fill out form, send data and find account link', () => {
+      cy.fixture('association.json').then((association) => {
+        cy.get('[type="checkbox"]').check();
+        cy.get('input[name=username]').type(association.username);
+        cy.get('input[name=email]').type(association.email);
+        cy.get('input[name=password]').type(association.password);
+        cy.get('.button_signup_page').click();
+        cy.contains('Account');
+        cy.contains('Create job');
+      });
     });
   });
 });

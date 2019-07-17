6866dd75ed05d99f07c7dba9e6691f4100236b3a
Flavor85 <flaviengr75@gmail.com>
2019-07-17 23:31:10 +0200

    profile_spec

---
+++ e2e/cypress/integration/profile_spec.js
@@ -0,0 +1,44 @@
+describe('Job operations CRUD by association', () => {
+  beforeEach(() => {
+    cy.fixture('association.json').as('association');
+  });
+  context('Account Users', () => {
+    beforeEach(() => {
+      cy.login('user');
+    });
+    it('Should dropdown button account', () => {
+      cy.contains('Account').click();
+      cy.contains('Logout');
+      cy.contains('Settings');
+      cy.contains('Profil');
+    });
+    it('Should logout user', () => {
+      cy.contains('Account').click();
+      cy.contains('Logout').click();
+      cy.contains('Signup');
+    });
+    it('Should click and direct profile page', () => {
+      cy.contains('Account').click();
+      cy.contains('Profile').click();
+      cy.url().should('contain', '/users/profile');
+    });
+  });
+  context('Account Association', () => {
+    beforeEach(() => {
+      cy.login('association');
+    });
+    it('Should have and click on My Job', () => {
+      cy.contains('Account').click();
+      cy.contains('My job').click();
+      cy.url().should('contain', '/users/job/list');
+    });
+    it('Should delete account', () => {
+      cy.contains('Account').click();
+      cy.contains('Settings').click();
+      cy.get('.btn_delete').click();
+      cy.get('input[name=password]').type('thesummerisComing?');
+      cy.contains('Confirm').click();
+      cy.contains('Signup');
+    });
+  });
+});

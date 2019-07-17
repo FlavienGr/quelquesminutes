755b7e4945e8f1a7f5f318cb191acfaf91eb7415
Flavor85 <flaviengr75@gmail.com>
2019-07-17 16:36:31 +0200

    login command cypress

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -1,25 +1,21 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (userType) => {
+  const types = {
+    association: {
+      username: 'Rain Fall',
+      email: 'shitweather@fake.io',
+      password: 'thesummerisComing?'
+    },
+    user: {
+      username: 'Thomas Edison',
+      email: 'edison@fake.io',
+      password: 'ibecamepresident'
+    }
+  };
+
+  // grab the user
+  const user = types[userType];
+  cy.exec('npm run db:delete && npm run db:seed');
+  cy.visit('/login');
+  cy.get('input[name=email]').type(user.email);
+  cy.get('input[name=password]').type(`${user.password}{enter}`);
+});
